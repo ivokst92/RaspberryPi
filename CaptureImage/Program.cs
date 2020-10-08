24ddea2b3e8d1bafeef36d6a72d71d758b7127d5
Ivo Krastev <ivohayday@gmail.com>
2020-10-08 22:55:39 +0300

    Add project files.

---
+++ CaptureImage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Camera;
+
+namespace CaptureImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Say cheese!");
+            CaptureImage();
+            Console.WriteLine("Captured!");
+        }
+
+        static void CaptureImage()
+        {
+            var settings = new CameraStillSettings
+            {
+                CaptureExposure = CameraExposureMode.Night,
+                CaptureWidth = 1920,
+                CaptureHeight = 1080,
+                CaptureJpegQuality = 100,
+                CaptureTimeoutMilliseconds = 300
+            };
+
+            var pictureBytes = Pi.Camera.CaptureImage(settings);
+            var targetPath = $"/home/pi/{Guid.NewGuid()}.jpg";
+            if (File.Exists(targetPath))
+                File.Delete(targetPath);
+
+            File.WriteAllBytes(targetPath, pictureBytes);
+            Console.WriteLine($"Took picture -- Byte count: {pictureBytes.Length}");
+        }
+    }
+}
---
+++ PIR.Sensor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace PIR.Sensor
+{
+    class Program
+    {
+        private const int PIR_PIN = 4;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing GPIO");
+
+            using (var gpio = new GpioController())
+            using (var cts = new CancellationTokenSource())
+            {
+                Console.CancelKeyPress += (s, e) => cts.Cancel();
+
+                gpio.OpenPin(PIR_PIN, PinMode.Input);
+
+                Console.WriteLine("Monitoring PIR sensor. ctrl+c to cancel.");
+                bool lastOn = false;
+
+                while (!cts.IsCancellationRequested)
+                {
+                    bool pirOn = gpio.Read(PIR_PIN) == true;
+
+                    if (lastOn != pirOn)
+                    {
+                        Console.WriteLine($"Motion sensor is now {(pirOn ? "on" : "off")}");
+                        lastOn = pirOn;
+                    }
+                }
+
+                Console.WriteLine("Cleaning up");
+                gpio.ClosePin(PIR_PIN);
+            }
+        }
+    }
+}
